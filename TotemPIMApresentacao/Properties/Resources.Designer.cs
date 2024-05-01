f4d7729ce2d4f4efdeeb956f4dff124a8cff5ca5
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-05-01 16:01:05 -0300

    Tela de menu certa

--- TotemPIMApresentacao/Properties/Resources.Designer.cs
+++ TotemPIMApresentacao/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace TotemPIMApresentacao.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Menu {
+            get {
+                object obj = ResourceManager.GetObject("Menu", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ResultadoQuestionario {
             get {
                 object obj = ResourceManager.GetObject("ResultadoQuestionario", resourceCulture);
